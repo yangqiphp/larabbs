c81cfec3ff459110e64a078909c9a6961d96fcd1
杨淇 <qi7.kevin@gmail.com>
2018-03-21 16:08:28 +0800

    Seeding topics and users data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(\Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(\App\Models\User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index) use($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'yangqi';
+        $user->email = 'yangqiphp@163.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+    }
+}
