6da75526387860621f2cf54ff747cade70e892ac
yangqiphp <qi7.kevin@gmail.com>
2018-03-22 09:55:27 +0000

    User can delete reply

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -15,6 +15,6 @@ class ReplyPolicy extends Policy
 
     public function destroy(User $user, Reply $reply)
     {
-        return true;
+        return $user->isAutorOf($reply) || $user->isAutorOf($reply->topic);
     }
 }
