621eb46f70d9f05d84b9c11e13fbe3fbf7453372
杨淇 <qi7.kevin@gmail.com>
2018-04-07 22:02:01 +0800

    add defult avatar and complete root page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes();
 
